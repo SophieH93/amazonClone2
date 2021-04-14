6b6deea60cb5a1e590e77157118027f7c127f94e
Sophie <sophiehickey123@gmail.com>
2021-04-14 10:35:51 +0100

    created home banner image

--- amazon-clone-cp/src/Home.js
+++ amazon-clone-cp/src/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css"
 
 export default function Home() {
     return (
-        <div>
-            
+        <div className="home">
+            <div className="home__container">
+                <img className="home__img" src="https://i.imgur.com/SYHeuYM.jpg" alt="" />
+                <div className="home__row">
+                    
+                </div>
+                <div className="home__row">
+
+                </div>
+                <div className="home__row">
+
+                </div>
+            </div>                        
         </div>
     )
 }
