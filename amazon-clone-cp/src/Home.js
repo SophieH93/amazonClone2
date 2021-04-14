ad6a7bae5e0426c4c5961d653307d638096f4740
Sophie <sophiehickey123@gmail.com>
2021-04-14 11:08:46 +0100

    added product component to home.js

--- amazon-clone-cp/src/Home.js
+++ amazon-clone-cp/src/Home.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import "./Home.css"
+import Product from "./Product";
 
-export default function Home() {
+function Home() {
     return (
         <div className="home">
             <div className="home__container">
                 <img className="home__img" src="https://i.imgur.com/SYHeuYM.jpg" alt="" />
                 <div className="home__row">
-                    
+                    <Product />
+                    <Product />
                 </div>
                 <div className="home__row">
-
+                    <Product />
+                    <Product />
+                    <Product />
                 </div>
                 <div className="home__row">
-
+                 <Product />
                 </div>
             </div>                        
         </div>
-    )
+    );
 }
+
+export default Home;
