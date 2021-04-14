89b724fbf743ce45c2c865469a41ca1df523d3c4
Sophie <sophiehickey123@gmail.com>
2021-04-14 11:08:04 +0100

    created product section hard code

--- amazon-clone-cp/src/Product.js
+++ amazon-clone-cp/src/Product.js
@@ -3,10 +3,21 @@ import "./Product.css"
 
 function Product() {
     return (
-        <div className="product-">
-            
-        </div>
-    )
+        <div className="product">
+            <div className="product__info">
+                <p>The lean startup</p>
+                <p className="product__price">
+                    <small>€</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>⭐</p>
+                </div>
+            </div>
+                <img src="https://m.media-amazon.com/images/I/51WIKlio9qL.jpg" alt=""/>
+                <button>Add to Basket</button>
+            </div>         
+    );
 }
 
 export default Product
