0028a4ad9ab93e4c6cc6871aa340d6e45de7f583
Sophie <sophiehickey123@gmail.com>
2021-04-15 21:04:42 +0100

    created useState to track email & password

--- amazon-clone-cp/src/Login.js
+++ amazon-clone-cp/src/Login.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Login.css";
 import {Link} from "react-router-dom";
 
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+
+    const [password, setPassword] = useState('');
+
+
+
     return (
         <div className="login">
             <Link to="/">
@@ -13,10 +20,10 @@ function Login() {
                 <h1>Sign In</h1>
                 <form>
                     <h5>Email</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
                     <button className="login__singInButton">Sign In</button>
                 </form>
