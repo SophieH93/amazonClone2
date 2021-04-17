1e4c3e4024172ce46f568e0ce6a97c150f2136fc
Sophie <sophiehickey123@gmail.com>
2021-04-17 17:33:41 +0100

    replaced glowing start with original in chechkoutP

--- amazon-clone-cp/src/CheckoutProduct.js
+++ amazon-clone-cp/src/CheckoutProduct.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
+import FlipMove from 'react-flip-move';
 
 
 function CheckoutProduct({id, image, title, price, rating, hideButton }) {
@@ -14,20 +15,21 @@ function CheckoutProduct({id, image, title, price, rating, hideButton }) {
     }
 
     return (
-        <div className="checkoutProduct">
+       
+            <div className="checkoutProduct">
             <img  className="checkoutProduct__image" src={image} alt="" />
 
             <div className="checkoutProduct__info">
                 <p className="checkoutProduct__title">{title}</p>
                 <p className="checkoutProduct__price">
-                    <small>€</small>
-                    <strong>{price}</strong>
+                    
+                    <strong>€{price}</strong>
                 </p>
                 <div className="checkoutProduct__rating">
                     {Array(rating)
                         .fill()
                         .map(() => (
-                        <p>🌟</p>
+                        <p>⭐</p>
                     ))}
                 </div>
                 {!hideButton && (
@@ -35,6 +37,9 @@ function CheckoutProduct({id, image, title, price, rating, hideButton }) {
                 )}
             </div>
         </div>
+
+
+      
     )
 }
 
