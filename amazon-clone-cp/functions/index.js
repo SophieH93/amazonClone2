073a94ae098a0b45c2c5d733f941610955036a37
Sophie <sophiehickey123@gmail.com>
2021-04-17 13:43:42 +0100

    created firebase funtion folder

--- amazon-clone-cp/functions/index.js
+++ amazon-clone-cp/functions/index.js
@@ -17,7 +17,7 @@ app.post('/payments/create', async (request, response) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: total,
-        currency: "usd",
+        currency: "eur",
     });
     response.status(201).send({
         clientSecret: paymentIntent.client_secret,
