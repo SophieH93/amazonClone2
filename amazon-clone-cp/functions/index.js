828bb0ecf6629b5146c4dbfbf88d9b29646ff6b5
Sophie <sophiehickey123@gmail.com>
2021-04-17 10:32:20 +0100

    created index.js in funcitons folder

---
+++ amazon-clone-cp/functions/index.js
@@ -0,0 +1,29 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51HFnRvHiDfU83hDh927Qxomio9nDtk1dRmix27gk7GaFhbcyE3zBtaZ4XENNYeSKGk7MpXBgEHeOJAK9APmA92bi00E7w6JRWq');
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => response.status(200).send('hello world'));
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('payment request received >>>>>>>>', total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/clone-cbb36/us-central1/api
