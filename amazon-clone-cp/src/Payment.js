387b04b5a1cd9470e7f78fa10f759cac115d535e
Sophie <sophiehickey123@gmail.com>
2021-04-17 13:39:28 +0100

    amended error in payment to uid

--- amazon-clone-cp/src/Payment.js
+++ amazon-clone-cp/src/Payment.js
@@ -46,7 +46,7 @@ function Payment() {
             },
         }).then(({paymentIntent}) => {
             // paymentIntent = payment confirmation 
-            db.collection('users').doc(user?.id).collection('orders').doc(paymentIntent.id).set({
+            db.collection('users').doc(user?.uid).collection('orders').doc(paymentIntent.id).set({
                 basket: basket,
                 amount: paymentIntent.amount,
                 created: paymentIntent.created
